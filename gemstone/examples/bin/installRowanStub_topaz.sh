eb85f8afb1869fde0708b8c3f065cbf491afcb89
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2025-08-13 15:31:53 -0700

    remove redundant invocation of RowanClassService_base.topaz

--- gemstone/examples/bin/installRowanStub_topaz.sh
+++ gemstone/examples/bin/installRowanStub_topaz.sh
@@ -29,4 +29,3 @@ fi
 
 export ROWAN_STUB_EXTENT_TYPE=base
 $GEMSTONE/examples/jadeite/bin/installRowanStub.topaz $topazCommandLine
-$GEMSTONE/examples/jadeite/gs/RowanClassService_base.topaz $topazCommandLine
